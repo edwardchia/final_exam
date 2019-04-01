c93a0bf02ebc16ad60d24c2af00ac538ef461c63
Edward Chia <chiaedward1@gmail.com>
2019-04-01 14:16:55 -0700

    created auctions controller and welcome controller

--- biddr_api/final-quiz/config/routes.rb
+++ biddr_api/final-quiz/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :auctions, only: [:new, :show, :index]
+
+  get("/", to: "welcome#home", as: :root)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
